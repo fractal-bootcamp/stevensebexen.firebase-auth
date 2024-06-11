8bde3fc1726c72ea3467c01d6ee89dcced79602c
Steven <ssebexen@gmail.com>
2024-06-11 12:05:53 -0400

    Set up front end

---
+++ src/SignupForm.tsx
@@ -0,0 +1,36 @@
+interface SignupFormProps {
+  onEmailInputChange: (newValue: string) => void
+  onPasswordInputChange: (newValue: string) => void
+  onSubmit: () => void
+  email: string
+  password: string
+}
+
+function SignupForm(props: SignupFormProps) {
+  return (
+    <div className='flex flex-row flex-1 justify-between gap-4'>
+      <input
+        className='flex-1 basis-2/5 p-1'
+        name='email'
+        placeholder='Email' 
+        value={props.email}
+        onChange={e => props.onEmailInputChange(e.target.value)}
+      />
+      <input
+        className='flex-1 basis-2/5 p-1'
+        name='password'
+        placeholder='Password'
+        value={props.password}
+        onChange={e => props.onPasswordInputChange(e.target.value)}
+      />
+      <button
+        className='flex-1 basis-1/5 rounded-md border border-indigo-900'
+        onClick={() => props.onSubmit()}
+      >
+        Log in
+      </button>
+    </div>
+  )
+}
+
+export default SignupForm;
